cd5a5448e1fae195e2bcd59a80e96ccf81f25a0d
Wiktor Lutowski <wlutowski@s201.local>
2024-12-02 07:30:47 +0100

    Add code-behind

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,22 @@ namespace DataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Produkt> ListaProduktow;
         public MainWindow()
         {
             InitializeComponent();
+            PrzygotujWiazanie();
+
+        }
+        private void PrzygotujWiazanie()
+        {
+            ListaProduktow = new ObservableCollection<Produkt>();
+            ListaProduktow.Add(new Produkt("01-11", "ołówek", 8, "Katowice 1"));
+            ListaProduktow.Add(new Produkt("PW-20", "pióro wieczne", 75, "Katowice 2"));
+            ListaProduktow.Add(new Produkt("DZ-10", "długopis żelowy", 1121, "Katowice 1"));
+            ListaProduktow.Add(new Produkt("DZ-12", "długopis kulkowy", 280, "Katowice 2"));
+            lstProdukty.ItemsSource = ListaProduktow;
+
         }
     }
 }
