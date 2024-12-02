54ed2986d67f27a7fc414ebba1badc40af845e65
Wiktor Lutowski <wlutowski@s201.local>
2024-12-02 07:35:49 +0100

    Add warehouse sort

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,12 +32,17 @@ namespace DataBinding
         private void PrzygotujWiazanie()
         {
             ListaProduktow = new ObservableCollection<Produkt>();
+
             ListaProduktow.Add(new Produkt("01-11", "ołówek", 8, "Katowice 1"));
             ListaProduktow.Add(new Produkt("PW-20", "pióro wieczne", 75, "Katowice 2"));
             ListaProduktow.Add(new Produkt("DZ-10", "długopis żelowy", 1121, "Katowice 1"));
             ListaProduktow.Add(new Produkt("DZ-12", "długopis kulkowy", 280, "Katowice 2"));
+
             lstProdukty.ItemsSource = ListaProduktow;
 
+
+            CollectionView widok = (CollectionView) CollectionViewSource.GetDefaultView(lstProdukty.ItemsSource); 
+            widok.SortDescriptions.Add(new SortDescription("Magazyn", ListSortDirection.Ascending));
         }
     }
 }
