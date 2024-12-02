0a5cb2d56d6d95f8770cd9a003941c3b70811e00
Wiktor Lutowski <wlutowski@s201.local>
2024-12-02 07:32:23 +0100

    Add produkt class

---
+++ Produkt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataBinding
+{
+
+    class Produkt
+    {
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int LiczbaSztuk { get; set; }
+        public string Magazyn { get; set; }
+
+        public Produkt(string sym, string naz, int szt, string mag)
+        {
+            Symbol = sym;
+            Nazwa = naz;
+            LiczbaSztuk = szt;
+            Magazyn = mag;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3}", Symbol, Nazwa, LiczbaSztuk, Magazyn);
+        }
+    }
+}
